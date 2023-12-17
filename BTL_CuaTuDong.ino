388e60ba856dce741eb41c6dc93179b689eafdb3
NXGiang22022002 <128494204+NXGiang22022002@users.noreply.github.com>
2023-12-17 14:43:41 +0700

    Update BTL_CuaTuDong.ino

--- BTL_CuaTuDong.ino
+++ BTL_CuaTuDong.ino
@@ -15,20 +15,18 @@ Servo myservo;
 #define OPEN_STATE  1
 #define CLOSE_STATE 2
 
-int distance;
-unsigned long previousMillis = 0;
-unsigned long autoMillis = 0;
-int modeRun = IDLE_STATE;
-unsigned long timeMillis = 0;
-unsigned int timeOpen = 2;
+int distance; //Lưu trữ giá trị khoảng cách đo được từ cảm biến siêu âm.
+unsigned long previousMillis = 0; //Lưu trữ giá trị khoảng cách đo được từ cảm biến siêu âm.
+unsigned long autoMillis = 0; //Theo dõi thời gian (đơn vị là milliseconds) khi có sự kiện tự động xảy ra.
+int modeRun = IDLE_STATE; //Xác định trạng thái hoạt động của chương trình (IDLE_STATE, OPEN_STATE, CLOSE_STATE).
+unsigned long timeMillis = 0;//Theo dõi thời gian (đơn vị là milliseconds) cho một sự kiện cụ thể.
+unsigned int timeOpen = 2; //Đặt thời gian (đơn vị là giây) cần để mở cửa servo.
 
 void setup() {
-  Serial.begin(115200);
-  Dist.begin(SRF04_ECHO_PIN, SRF04_TRIG_PIN);
-  myservo.attach(SERVO_PIN);
-  myservo.write(SERVO_CLOSED_ANGLE);
-  pinMode(13, OUTPUT);
-  pinMode(A7, INPUT_PULLUP);
+  Serial.begin(115200);//Khởi tạo giao tiếp serial với tốc độ baud là 115200. 
+  Dist.begin(SRF04_ECHO_PIN, SRF04_TRIG_PIN);//Khởi tạo đối tượng DistanceSRF04 (Dist) 
+  myservo.attach(SERVO_PIN);//Kết nối động cơ Servo (myservo) với chân đã được chỉ định (SERVO_PIN). 
+  myservo.write(SERVO_CLOSED_ANGLE);//Góc ban đầu của servo được đặt thành SERVO_CLOSED_ANGLE (180 độ).
 }
 
 void loop() {
